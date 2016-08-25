325b87bef4a285e96cd369a32ca1b26bc387469c
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2016-08-26 00:30:11 +0800

    Slightly tweak the src code

--- src/module1.h
+++ src/module1.h
@@ -1,4 +1,9 @@
 #include<stdio.h>
 
+/*
+ * Module 1 : Algorithms not using any significant data structures
+ *
+ */
 int MaxSubsequenceSum1( const int A[], int N);
 int MaxSubsequenceSum2( const int A[], int N);
+int MaxSubsequenceSum3( const int A[], int N);
--- src/utility.c
+++ src/utility.c
@@ -1,9 +1,20 @@
 #include "utility.h"
 
+/*
+ * Idea    : In boolean `AND` operation such as `x && y`, `y` is 
+ *           evaluated iff `x` is true. If `x` is false, then 
+ *           `y` is not evaluated because the whole expression would
+ *           be false which can be deduced without even evaluating `y`.
+ *           This is called short-circuiting when the value of a boolean
+ *           expression can be deduced without evaluating all operands in it.
+ * Big-O   :
+ * Remarks : http://stackoverflow.com/questions/7074010/find-maximum-of-three-number-in-c-without-using-conditional-statement-and-ternar
+ */
+
 int Max3( int A, int B, int C )
 {
   int m = A;
-  ( m < B ) && ( m = B );
-  ( m < C ) && ( m = C );
+  ( void )( ( m < B ) && ( m = B ) ); // set `m` to `B` iff `m` is less than `B`
+  ( void )( ( m < C ) && ( m = C ) );
   return m;
 }
